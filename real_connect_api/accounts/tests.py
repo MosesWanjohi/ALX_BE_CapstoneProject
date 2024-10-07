5bedc6262ecb9a88fc1bf7a399d6c8965f7ffe43
Moses Muturi <wanjohimuturi.tech@gmail.com>
2024-10-08 02:07:15 +0300

    Created TestCase for user registration in accounts.py

--- real_connect_api/accounts/tests.py
+++ real_connect_api/accounts/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import CustomUser, Role
 
-# Create your tests here.
+class UserRegistrationTestCase(APITestCase):
+    def setUp(self):
+        # Create a role for the user
+        Role.objects.create(name='regular')
+
+    def test_user_registration(self):
+        url = reverse('register')  
+        data = {
+            'username': 'testuser',
+            'email': 'testuser@example.com',
+            'password': 'testpassword123',
+        }
+        
+        #Send a POST request to the registration endpoint
+        response = self.client.post(url, data, format='json')
+
+        #Check that the user is created successfully
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED) 
+        
+        #Check that the user was created in the database
+        self.assertTrue(CustomUser.objects.filter(username='testuser').exists())  # Check that the user exists in the database
+
+    def test_user_registration_missing_fields(self):
+        url = reverse('register')
+        data = {
+            'username': '',
+            'email': 'testuser@example.com',
+            'password': 'testpassword123',
+        }
+        response = self.client.post(url, data, format='json')
+       
+       # Check for bad request due to missing username
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)  
